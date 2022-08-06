1f02be00921623071ce1b013c133c3cbc378df72
gb <107445919+rtsuki@users.noreply.github.com>
2022-08-06 14:17:19 -0300

    Update Event.ts

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -4,7 +4,7 @@ import type { Client } from '../Client';
 export abstract class Event <EventClient = Client> {
   eventName: string;
 
-  execute(client: EventClient, ...args: unknown[]): Awaitable<any> {
+  execute(client: EventClient, ...args: unknown[]): Awaitable<void> {
     return { client, args };
   }
 }
